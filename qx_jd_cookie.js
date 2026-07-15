155318b2d1f25af136be9f479126f31077b97b8c
xiaotaiye88 <xiaotaiye88@users.noreply.github.com>
2026-07-15 18:08:19 +0800

    添加弹窗通知

--- qx_jd_cookie.js
+++ qx_jd_cookie.js
@@ -8,6 +8,7 @@ if (ki >= 0) { key = c.substring(ki + 7).split(';')[0]; }
 
 if (pin && key) {
   console.log('[JD-COOKIE] pt_pin=' + pin + ';pt_key=' + key + ';');
+  $notify('京东Cookie', pin, key);
 } else if (c.length > 0) {
   console.log('[JD-COOKIE] 有cookie但无pt: ' + c.substring(0, 120));
 }
