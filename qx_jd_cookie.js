b6d3019caa00a7a92a44014615cd98338cd21e61
xiaotaiye88 <xiaotaiye88@users.noreply.github.com>
2026-07-15 21:39:30 +0800

    去掉persistentStore，仅保留notify

--- qx_jd_cookie.js
+++ qx_jd_cookie.js
@@ -8,14 +8,8 @@ if (pi >= 0) pin = c.substring(pi + 7).split(';')[0];
 if (ki >= 0) key = c.substring(ki + 7).split(';')[0];
 
 if (pin && key) {
-  const full = 'pt_key=' + key + ';pt_pin=' + pin + ';';
-  const last = $persistentStore.read('jd_ts_' + pin) || '0';
-  if ((Date.now() - parseInt(last)) > 300000) {
-    $persistentStore.write(String(Date.now()), 'jd_ts_' + pin);
-    $persistentStore.write(full, 'jd_last');
-    console.log('[JD-COOKIE] ' + full);
-    $notify('京东Cookie', pin, key.substring(0, 50) + '...');
-  }
+  console.log('[JD-COOKIE] ' + 'pt_key=' + key + ';pt_pin=' + pin + ';');
+  $notify('京东Cookie', pin, key);
 }
 
 $done({body: body});
