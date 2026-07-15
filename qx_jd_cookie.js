8b0d16246f37e0fc1701723668bc924c7ff06088
xiaotaiye88 <xiaotaiye88@users.noreply.github.com>
2026-07-15 21:44:36 +0800

    恢复：仅日志输出，移除所有API调用

--- qx_jd_cookie.js
+++ qx_jd_cookie.js
@@ -1,24 +1,17 @@
 const c = $request.headers['Cookie'] || $request.headers['cookie'] || '';
 
-let pin = '', key = '';
+let pin = '', key = '', ws = '';
 const pi = c.indexOf('pt_pin=');
 const ki = c.indexOf('pt_key=');
+const wi = c.indexOf('wskey=');
 if (pi >= 0) pin = c.substring(pi + 7).split(';')[0];
 if (ki >= 0) key = c.substring(ki + 7).split(';')[0];
+if (wi >= 0) ws = c.substring(wi + 6).split(';')[0];
 
 if (pin && key) {
-  console.log('[JD-COOKIE] ' + 'pt_key=' + key + ';pt_pin=' + pin + ';');
-
-  // 尝试推送到本地同步服务（如果运行的话）
-  try {
-    $httpClient.post('http://192.168.19.192:3456/', {
-      'Content-Type': 'text/plain'
-    }, 'pt_key=' + key + ';pt_pin=' + pin + ';', function(err, resp, data) {
-      if (!err) console.log('[JD-COOKIE] 推送成功'); else console.log('[JD-COOKIE] 推送失败');
-    });
-  } catch(e) {
-    console.log('[JD-COOKIE] httpClient不可用: ' + e);
-  }
+  console.log('[JD] pt_key=' + key + ';pt_pin=' + pin + ';');
 }
-
+if (ws) {
+  console.log('[JD] wskey=' + ws + ';' + (pin ? 'pin=' + pin : ''));
+}
 $done({});
