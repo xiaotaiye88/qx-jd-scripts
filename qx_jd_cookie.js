e1ca0101b752fb113582610574e0ba530573517d
xiaotaiye88 <xiaotaiye88@users.noreply.github.com>
2026-07-15 21:42:21 +0800

    测试可用性，退回response-header

--- qx_jd_cookie.js
+++ qx_jd_cookie.js
@@ -1,5 +1,4 @@
 const c = $request.headers['Cookie'] || $request.headers['cookie'] || '';
-const body = $response.body || '';
 
 let pin = '', key = '';
 const pi = c.indexOf('pt_pin=');
@@ -9,7 +8,17 @@ if (ki >= 0) key = c.substring(ki + 7).split(';')[0];
 
 if (pin && key) {
   console.log('[JD-COOKIE] ' + 'pt_key=' + key + ';pt_pin=' + pin + ';');
-  $notify('京东Cookie', pin, key);
+
+  // 尝试推送到本地同步服务（如果运行的话）
+  try {
+    $httpClient.post('http://192.168.19.192:3456/', {
+      'Content-Type': 'text/plain'
+    }, 'pt_key=' + key + ';pt_pin=' + pin + ';', function(err, resp, data) {
+      if (!err) console.log('[JD-COOKIE] 推送成功'); else console.log('[JD-COOKIE] 推送失败');
+    });
+  } catch(e) {
+    console.log('[JD-COOKIE] httpClient不可用: ' + e);
+  }
 }
 
-$done({body: body});
+$done({});
