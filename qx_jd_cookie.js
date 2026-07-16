7348422db030bdea0b9c930ae8dc7b3fe330d71e
xiaotaiye88 <xiaotaiye88@users.noreply.github.com>
2026-07-16 09:50:59 +0800

    wskey抓取优化：兼容pin=格式，输出带pt_pin

--- qx_jd_cookie.js
+++ qx_jd_cookie.js
@@ -2,9 +2,11 @@ const c = $request.headers['Cookie'] || $request.headers['cookie'] || '';
 
 let pin = '', key = '', ws = '';
 const pi = c.indexOf('pt_pin=');
+const p2 = c.indexOf('pin=');
 const ki = c.indexOf('pt_key=');
 const wi = c.indexOf('wskey=');
 if (pi >= 0) pin = c.substring(pi + 7).split(';')[0];
+else if (p2 >= 0) pin = c.substring(p2 + 4).split(';')[0];
 if (ki >= 0) key = c.substring(ki + 7).split(';')[0];
 if (wi >= 0) ws = c.substring(wi + 6).split(';')[0];
 
@@ -12,6 +14,6 @@ if (pin && key) {
   console.log('[JD] pt_key=' + key + ';pt_pin=' + pin + ';');
 }
 if (ws) {
-  console.log('[JD] wskey=' + ws + ';' + (pin ? 'pin=' + pin : ''));
+  console.log('[JD] wskey=' + ws + ';' + (pin ? 'pt_pin=' + pin + ';' : ''));
 }
 $done({});
